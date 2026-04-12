21b36b0f02000fc9b8fbf06d3115e3ef7de30a3f
egolam <eyavuz1273@hotmail.com>
2026-04-13 02:45:02 +0300

    node version

---
+++ lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+import { admin, anonymous } from "better-auth/plugins";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: false,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  plugins: [anonymous(), admin(), nextCookies()],
+});
